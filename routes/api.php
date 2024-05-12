b184277a5e02175c67d3ea58acb474b6cfca13aa
Vincent Paulin <ecniv81@gmail.com>
2024-05-12 20:22:49 +0200

    feat: fix endpoint url

--- routes/api.php
+++ routes/api.php
@@ -30,6 +30,6 @@ Route::middleware(['auth:sanctum', AdminMiddleware::class])->group(function () {
     Route::get('/admin/doctor/stays', [AdminController::class, 'getStaysByDoctorMatricule']);
     Route::get('/admin/doctor-agenda', [AdminController::class, 'getAgendaByDoctorMatricule']);
     Route::get('/admin/stay-not-programed', [AdminController::class, 'getAllStayNotProgramed']);
-    Route::post('/admin/create-createAppointment', [AdminController::class, 'createAppointment']);
+    Route::post('/admin/create-appointment', [AdminController::class, 'createAppointment']);
     Route::get('/admin/get-appointments-starting-today', [AdminController::class, 'getAppointmentsStartingToday']);
 });
